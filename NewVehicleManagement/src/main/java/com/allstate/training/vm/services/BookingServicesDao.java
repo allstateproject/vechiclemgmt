a578bc1de4ede0b16de33e7fe0b41c978b5f04da
PrateekTD <prateek.t.d@gmail.com>
2019-09-19 11:07:19 +0530

    Updated BookingServicesDao and implementation

--- NewVehicleManagement/src/main/java/com/allstate/training/vm/services/BookingServicesDao.java
+++ NewVehicleManagement/src/main/java/com/allstate/training/vm/services/BookingServicesDao.java
@@ -4,19 +4,20 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.allstate.training.vm.entities.Booking;
+import com.allstate.training.vm.exceptions.BuisnessException;
 
 public interface BookingServicesDao{
-	public void addBooking(Booking boo);
-	public void updateBooking(Booking boo);
-	public void deleteBooking(String id);
-	public Booking getBookingById(String id);
-	public List<Booking> getAll();
-	public List<Booking> getBookingByPassengerId(String id);
-	public List<Booking> getBookingByBusId(String id);
-	public List<Booking> getBookingByRouteId(String id);
-	public List<Booking> getBookingByModeOfPayment(String mod);
-	public List<Booking> getBookingBetweenTime(LocalDateTime start,LocalDateTime end);
-	public List<Booking> getBookingBySource(String source);
-	public List<Booking> getBookingByDestination(String destination);
-	public List<Booking> getBookingBySourceAndDestination(String source,String destination);
+	public void addBooking(Booking boo)throws BuisnessException;
+	public void updateBooking(Booking boo)throws BuisnessException;
+	public void deleteBooking(String id)throws BuisnessException;
+	public Booking getBookingById(String id)throws BuisnessException;
+	public List<Booking> getAll()throws BuisnessException;
+	public List<Booking> getBookingByPassengerId(String id)throws BuisnessException;
+	public List<Booking> getBookingByBusId(String id)throws BuisnessException;
+	public List<Booking> getBookingByRouteId(String id)throws BuisnessException;
+	public List<Booking> getBookingByModeOfPayment(String mod)throws BuisnessException;
+	public List<Booking> getBookingBetweenTime(LocalDateTime start,LocalDateTime end)throws BuisnessException;
+	public List<Booking> getBookingBySource(String source)throws BuisnessException;
+	public List<Booking> getBookingByDestination(String destination)throws BuisnessException;
+	public List<Booking> getBookingBySourceAndDestination(String source,String destination)throws BuisnessException;
 }
